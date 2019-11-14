c966d01d3f4c1e7c7d803f7570c06d03e749469c
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2019-11-14 22:07:12 +0100

    updated main

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import rtde_control
-import rtde_io
 from pymodbus.client.sync import ModbusTcpClient
-import random
 
 from mes import Mes
 import rsd_conf
@@ -9,64 +7,64 @@ import q
 
 
 def main():
-    #mes = Mes(rsd_conf.MES_SERVER_URL)
+    mes = Mes(rsd_conf.MES_SERVER_URL)
     ur_ctrl = rtde_control.RTDEControlInterface(rsd_conf.UR_IP)
-    #ur_io = rtde_io.RTDEIOInterface(rsd_conf.UR_IP)
-    #ur_io.setStandardDigitalOut(rsd_conf.IO_GRIPPER, False)
-    ur_ctrl.moveJ(q.IDLE)
-
     ur_modbus_client = ModbusTcpClient(rsd_conf.UR_IP)
-    ur_modbus_client.write_coil(16 + rsd_conf.IO_GRIPPER, False)
+    pi_modbus_client = ModbusTcpClient(rsd_conf.PI_IP, rsd_conf.PI_MODBUS_PORT)
+    move = ur_ctrl.moveJ
 
-    # pi_modbus_client = ModbusTcpClient(rsd_conf.PI_IP)
+    def grip(do_grip=True):
+        ur_modbus_client.write_coil(16 + rsd_conf.IO_GRIPPER, do_grip)
 
-    def gripper_grasp(grasp_q, pre_post_q):
-        ur_ctrl.moveJ(pre_post_q)
-        ur_ctrl.moveJ(grasp_q)
-        #ur_io.reconnect()
-        #ur_io.setStandardDigitalOut(rsd_conf.IO_GRIPPER, True)
-        ur_modbus_client.write_coil(16 + rsd_conf.IO_GRIPPER, True)
-        ur_ctrl.moveJ(pre_post_q)
+    def release():
+        grip(False)
 
-    def gripper_release():
-        #ur_io.reconnect()
-        #ur_io.setStandardDigitalOut(rsd_conf.IO_GRIPPER, False)
-        ur_modbus_client.write_coil(16 + rsd_conf.IO_GRIPPER, False)
+    def get_vision_color():
+        values = pi_modbus_client.read_input_registers(0, 1)
+        return values.registers[0]
+
+    move(q.IDLE)
+    release()
 
     while True:
-        # order, ticket = mes.take_a_ready_order()
-        order = {"blue": 1, "yellow": 1, "red": 1}
+        order, ticket = mes.take_a_ready_order()
+        print(order)
 
         # pack the order in a box
-        for brick_id in range(3):
-            brick_color = rsd_conf.BRICK_COLORS[brick_id]
-            q_grasp_brick, q_above_brick = q.GRASP_BRICKS[brick_id], q.ABOVE_BRICKS[brick_id]
+        for brick_color_id in range(len(rsd_conf.BRICK_COLORS)):
+            brick_color = rsd_conf.BRICK_COLORS[brick_color_id]
+            q_grasp_brick, q_above_brick = q.GRASP_BRICKS[brick_color_id], q.ABOVE_BRICKS[brick_color_id]
 
             count = order[brick_color]
             while count > 0:
-                gripper_grasp(q_grasp_brick, q_above_brick)
-                ur_ctrl.moveJ(q.ABOVE_CAMERA)
-                brick_has_right_color = random.random() < 0.8  # pi_modbus_client.read_coils(brick_id)
-                if brick_has_right_color:
+                move(q_above_brick)
+                move(q_grasp_brick)
+                grip()
+                move(q_above_brick)
+
+                move(q.ABOVE_CAMERA)
+                if get_vision_color() == brick_color_id:
                     ur_ctrl.moveJ(q.BRICK_DROP_ORDER_BOX)
                     count -= 1
                 else:
                     ur_ctrl.moveJ(q.BRICK_DROP_DISCARD_BOX)
-                gripper_release()
+                release()
 
         # TODO: Request mir and wait for it to come.
 
         # move the box onto the mir
-        gripper_grasp(q.GRASP_ORDER_BOX_EDGE, q.ABOVE_ORDER_BOX_EDGE)
-        ur_ctrl.moveJ(q.ABOVE_ORDER_BOX_EDGE_WITH_BOX)
-        ur_ctrl.moveJ(q.ORDER_BOX_DROP_MIR_WAYPOINT)
-        ur_ctrl.moveJ(q.ORDER_BOX_DROP_MIR)
-        gripper_release()
-        ur_ctrl.moveJ(q.IDLE)
+        move(q.ABOVE_ORDER_BOX_EDGE)
+        move(q.GRASP_ORDER_BOX_EDGE)
+        grip()
+        move(q.ABOVE_ORDER_BOX_EDGE_WITH_BOX)
+        move(q.ORDER_BOX_DROP_MIR_WAYPOINT)
+        move(q.ORDER_BOX_DROP_MIR)
+        release()
+        move(q.IDLE)
 
         # TODO: Let the mir know, we're done
 
-        # mes.complete_order(order, ticket)
+        mes.complete_order(order, ticket)
 
 
 if __name__ == '__main__':
