d9c4e3bc4c22561f308f1d69d0953bbde308d244
soren <sorenholmpetersen@gmail.com>
2019-11-15 15:04:51 +0100

    Added read and write to PLC register functions.

--- mir/rest_api_functions.py
+++ mir/rest_api_functions.py
@@ -2,9 +2,10 @@ import requests
 import json
 import time
 
+token = "Basic UlNEMjAxOTo0NWVkZWQ4YmRjMjJmZGQ5ZjcwOTJmY2RhMDhkOWFmMjdjNDNhNjQ4MWFkZDYyMDgwZWI3MmM0OTcyOTQ1NzZl"
+
 def get_mission_guid(mission_name):         
     url = 'http://mir.com/api/v2.0.0/missions'
-    token = 'Basic RGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZmMDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGNmOTI5NzkyODM0YjAxNA=='
     r = requests.get(url, headers={'Authorization': token}) #stackoverflow: Python request how to pass auth header
     if(r.status_code == 200):   
         r_json = r.json()
@@ -20,7 +21,6 @@ def get_mission_guid(mission_name):
 
 """Returns an integer (id)"""
 def add_to_mission_queue(guid):
-    token = 'Basic RGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZmMDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGNmOTI5NzkyODM0YjAxNA=='
     url = "http://mir.com/api/v2.0.0/mission_queue"
 
     body = {"mission_id": guid}
@@ -40,7 +40,6 @@ def add_to_mission_queue(guid):
 """Input: the id, which was created when 
    the mission was posted to the queue"""
 def remove_mission(id):
-    token = 'Basic RGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZmMDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGNmOTI5NzkyODM0YjAxNA=='
     url = "http://mir.com/api/v2.0.0/mission_queue/" + str(id)
 
     r = requests.delete(url, headers={'Authorization' : token})
@@ -52,9 +51,37 @@ def remove_mission(id):
         print(r.status_code)
         print(r.content)
 
+
+
+def get_register_value(register_id):
+    url = "http://mir.com/api/v2.0.0/registers"
+    r = requests.get(url, headers={'Authorization' : token})
+
+    if(r.status_code == 200):
+        r_json = r.json()
+        for elem in r_json:
+            if elem['id'] == register_id:
+                value = elem['value']
+        print("Register " + str(register_id) + ": " + str(value))
+        return value
+    else:
+        print(r.status_code)
+        print(r.content)
+
+def set_register_value(register_id, value):
+    url = "http://mir.com/api/v2.0.0/registers/" + str(register_id)
+    body = {"value" : value, "label" : ""}
+    r = requests.put(url, headers={'Authorization' : token, 'content-type':'application/json'}, data = json.dumps(body))
+
+    if(r.status_code == 200):
+        print("Set register " + str(register_id) + ": " + str(value))
+    else:
+        print(r.status_code)
+        print(r.content)
+
+
 def get_status():
     url = "http://mir.com/api/v2.0.0/status"
-    token = 'Basic RGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZmMDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGNmOTI5NzkyODM0YjAxNA=='
     r = requests.get(url, headers={'Authorization' : token})
 
     if(r.status_code == 200):
@@ -64,15 +91,22 @@ def get_status():
     else:
         print("Could not get status")
 
+# set_register_value(9,101)
+# get_register_value(9)
+# set_register_value(9,1337)
+# get_register_value(9)
 
-guid = get_mission_guid("shp_mission")
-queue_id = add_to_mission_queue(guid)
 
-time.sleep(5)
-remove_mission(queue_id)
+# guid = get_mission_guid("shp_mission")
+# queue_id = add_to_mission_queue(guid)
 
 # while True:
 #     time.sleep(1)
 #     r_json = get_status()
 #     print("mode: " + str( r_json['mode_id'] ))
 #     print("state_text: " +  r_json['state_text'])
+#     get_register_value(9)
+
+
+
+
