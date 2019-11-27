4b240e731b4cecf374682cb144fa6c4aeacd4c77
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2019-11-27 16:51:50 +0100

    added services to rsdredis

--- rsd/utils/rsd_redis.py
+++ rsd/utils/rsd_redis.py
@@ -7,12 +7,9 @@ class RsdRedis:
     def __init__(self, parser=pickle):
         self.r = redis.Redis()
         self.p = parser
-        self.sub_threads = {}
+        self.threads = {}
         self.sub_i = 0
         self.pubsub = {}
-
-    def publish(self, channel, data):
-        self.r.publish(channel, self.p.dumps(data))
 
     def get(self, key):
         val = self.r.get(key)
@@ -22,6 +19,9 @@ class RsdRedis:
 
     def set(self, key, val):
         self.r.set(key, self.p.dumps(val))
+
+    def publish(self, channel, data):
+        self.r.publish(channel, self.p.dumps(data))
 
     def subscribe(self, channel, cb, sync=True, blocking=False):
         sub_i = self.sub_i
@@ -43,10 +43,10 @@ class RsdRedis:
 
         t = threading.Thread(target=sub)
         t.start()
-        self.sub_threads[sub_i] = t
+        self.threads[sub_i] = t
 
         if blocking:
-            t.join()
+            self.join(sub_i)
 
         return sub_i
 
@@ -59,20 +59,55 @@ class RsdRedis:
             for i in list(self.pubsub.keys()):
                 self.unsubscribe(i)
 
-    def join(self):
-        for t in self.sub_threads.values():
-            t.join()
+    def join(self, sub_i=None):
+        if sub_i is None:
+            for i in list(self.threads.keys()):
+                self.join(i)
+        else:
+            self.threads[sub_i].join()
+            del self.threads[sub_i]
+
+    def service(self, name, cb, sync=True, blocking=False):
+        def _cb(service_data):
+            req_id, cb_data = service_data
+            response = cb(cb_data)
+            self.publish("res:{}".format(req_id), response)
+
+        return self.subscribe("service:{}".format(name), _cb, sync, blocking)
+
+    def service_call(self, name, args):
+        req_id = self.r.incr("req:id")
+        obj = {}
+
+        def store_unsub(val):
+            obj["val"] = val
+            self.unsubscribe(sub)
+
+        sub = self.subscribe("res:{}".format(req_id), store_unsub, blocking=False)
+        self.publish("service:{}".format(name), (req_id, args))
+        self.join(sub)
+        return obj.get("val", None)
 
 
 def main():
     import time
     r = RsdRedis()
+    print("pub sub test:")
     r.subscribe("hello", lambda data: print("hello", data))
     r.publish("hello", "world")
 
-    time.sleep(0.1)
+    time.sleep(.1)
     r.unsubscribe()
-    print("test finished")
+
+    print("\nservice test:")
+    r.service("mul2", lambda x: x * 2)
+    val = r.service_call("mul2", 3)
+    print(val)
+
+    time.sleep(.1)
+    r.unsubscribe()
+
+    print("\ntest finished")
 
 
 if __name__ == '__main__':
